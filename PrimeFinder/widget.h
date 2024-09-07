f6c176bf9bbf05004f902f6eda33864c7c24b762
mitchcamza <192mitch88@gmail.com>
2024-09-07 11:27:47 -0700

    Design and implement GUI

--- PrimeFinder/widget.h
+++ PrimeFinder/widget.h
@@ -3,6 +3,14 @@
 
 #include <QWidget>
 
+class QLabel;
+class QPlainTextEdit;
+class QPushButton;
+class QSpinBox;
+
+const int MAX_THREADS = 4; // Maximum number of allowed threads
+
+
 class Widget : public QWidget
 {
     Q_OBJECT
@@ -10,5 +18,24 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+
+private slots:
+    void startPrimeSearch();
+
+private:
+    QLabel *labelStart;
+    QLabel *labelEnd;
+    QLabel *labelThreads;
+
+    QSpinBox *spinBoxStart;
+    QSpinBox *spinBoxEnd;
+    QSpinBox *spinBoxThreads;
+
+    QPushButton *buttonStart;
+    QPushButton *buttonStop;
+
+    QPlainTextEdit *primeDisplay[4]; // For displaying primes found by each thread
+    void setupUI();
 };
+
 #endif // WIDGET_H
